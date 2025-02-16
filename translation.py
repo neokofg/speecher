7d953a97fc8d2049b9ffed8f58ba512666ce868c
neoko <33611354+neokofg@users.noreply.github.com>
2025-02-16 15:16:59 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -42,7 +42,7 @@ class TranslationModel:
 
         outputs = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
-        translation = outputs[0].split(f"{target_lang}: ")[-1].strip()
+        translation = outputs[0]
         
         return translation
 

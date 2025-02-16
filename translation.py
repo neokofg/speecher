360a8ac66ffe23c625bf59049a168ba6993aebe1
neoko <33611354+neokofg@users.noreply.github.com>
2025-02-16 15:42:24 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -23,7 +23,7 @@ class TranslationModel:
 
     def translate(self, text, source_lang, target_lang):
         prompt_template = PromptTemplate.from_template(
-            f"Translate words from {source_lang} to {target_lang}:\n{source_lang}: {{input_text}}\n{target_lang}:"
+            f"Translate words from {source_lang} to {target_lang}:\n{source_lang}: {{input_text}}\n{target_lang}: "
         )
         prompt = prompt_template.format(input_text=text)
 
